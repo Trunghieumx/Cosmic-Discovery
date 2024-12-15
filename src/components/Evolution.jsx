1748d3f0c130140d5dac82000686a765524d7736
BomftT <tuanminhcnttk08@gmail.com>
2024-12-15 12:47:13 +0700

    change components

---
+++ src/components/Evolution.jsx
@@ -0,0 +1,10 @@
+import './Evolution.css'
+function Evolution() {
+    return ( 
+        <div>
+            
+        </div>
+    );
+}
+
+export default Evolution;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import './Navbar.css'
 function Navbar() {
     return ( 
-        <>
-            
-        </>
+        <div className="nav">
+            <div className="icon-container">
+                <img src="./images/Home Icon.png" alt="Home" />
+                <img src="./images/observatory.png" alt="Observatory" />
+                <img src="./images/World Icon.png" alt="Planet" />
+                <img src="./images/Map Icon.png" alt="Map" />
+                <img src="./images/Astronaut Icon.png" alt="" />
+            </div>
+            <div className="eclipse">
+                <img src="/images/Ellipse.png" alt="" />
+            </div>
+        </div>
      );
 }
 
