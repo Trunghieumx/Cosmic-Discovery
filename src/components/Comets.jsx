fdc9f2611cfe3c8e34759fd77d289e816d6c700c
Trunghieumx <nguyenhieutrung860@gmail.com>
2024-12-24 21:32:36 +0700

    up comets

---
+++ src/components/Comets.jsx
@@ -0,0 +1,17 @@
+import './Comets.css'
+function Comets() {
+    return ( 
+        <>
+        <div className="comet">
+                <div className="title">Comets</div>
+                <div className="content">Comets are small celestial bodies in the Solar System, made mainly of ice, dust, gas, and organic matter. As they approach the Sun, the ice in comets evaporates and creates a characteristic bright tail, helping them stand out and be seen from Earth.</div>
+                <div className="title">How do comets form?</div>
+                <div className="content">
+                Comets form from dust, rock and ice in cold regions far from the Sun, such as the Oort Cloud or the Kuiper Belt, through the accumulation of material. When pulled by gravity into orbit near the Sun, the ice sublimates to form gas clouds and bright tails.</div>
+            </div>
+        
+        </>
+     );
+}
+
+export default Comets;
